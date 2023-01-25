9d7017645ff2da9a796e5587d4317b4f75eecb55
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-25 18:00:37 -0300

    feat: cria testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    """Test arguments with wrong types"""
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message("string", "1")
+
+    """Test key is not a valid positive index of message"""
+    assert encrypt_message("string", 6) == "".join(reversed("string"))
+
+    """Test key is odd"""
+    assert encrypt_message("string", 3) == "rts_gni"
+    assert encrypt_message("string", 3) != "gni_rts"
+
+    """Test key is even"""
+    assert encrypt_message("string", 2) == "gnir_ts"
