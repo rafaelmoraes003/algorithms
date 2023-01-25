feb823266159845a8669623497ca0c9e78d87efe
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-25 17:21:34 -0300

    feat: implementa o algoritmo study_schedule, que descobre o melhor horário para disponibilizar os conteúdos de estudo de uma empresa

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time and target_time != 0:
+        return None
+
+    students_studying = 0
+
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if start <= target_time <= end:
+            students_studying += 1
+
+    return students_studying
+
+
+# print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
