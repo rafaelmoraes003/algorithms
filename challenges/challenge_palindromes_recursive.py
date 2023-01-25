6a4321cf08ead6ff73cc606f0d95e1c2008bc5f7
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-25 18:30:30 -0300

    fix: arruma caso base da função

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,7 +8,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index == high_index:
         return True
 
-    if (low_index - high_index) == 1:
+    if (high_index - low_index) == 1:
         return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
