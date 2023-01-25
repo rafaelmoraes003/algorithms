7330ed135b0defd626f67dc0ac7fe391c3a16e2c
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-25 20:49:19 -0300

    feat: implementa a função find_duplicate, que ordena uma lista e retorna o número repetido (se houver)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
+from challenges.challenge_anagrams import merge_sort
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if(
+        not nums 
+        or isinstance(nums[0], str)
+        or nums[0] < 0
+    ):
+        return False
+
+    merge_sort(nums)
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
